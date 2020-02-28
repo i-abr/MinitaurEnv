3c20ebdac3543bf24c978ad9b78e281aae52e52c
Ian Abraham <iabr4073@gmail.com>
2020-02-28 16:22:47 -0600

    double checked that the files work

---
+++ minitaur_env.py
@@ -0,0 +1,109 @@
+import numpy as np
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+from gym.spaces import Box
+
+from quatmath import mat2euler, mat2quat, euler2quat
+
+class MinitaurEnv(object):
+
+
+    def __init__(self, frame_skip=20, viewer=True):
+
+        self.frame_skip = frame_skip
+
+        model_path = './minitaur_run.xml'
+
+        self.model = load_model_from_path(model_path)
+        self.sim   = MjSim(self.model, nsubsteps=frame_skip)
+        self.data = self.sim.data
+        if viewer:
+            self.viewer = MjViewer(self.sim)
+
+        self.chassis_bid = self.model.body_name2id('base_chassis_link')
+        self.motor_bids  = [
+                self.model.get_joint_qpos_addr('motor_back_leftL_joint'),
+                self.model.get_joint_qpos_addr('motor_back_rightR_joint'),
+                self.model.get_joint_qpos_addr('motor_front_leftL_joint'),
+                self.model.get_joint_qpos_addr('motor_front_rightR_joint')
+            ]
+
+        self.extension_bids  = [
+                self.model.get_joint_qpos_addr('motor_back_leftL_extension'),
+                self.model.get_joint_qpos_addr('motor_back_rightR_extension'),
+                self.model.get_joint_qpos_addr('motor_front_leftL_extension'),
+                self.model.get_joint_qpos_addr('motor_front_rightR_extension')
+            ]
+
+
+        self.act_mid = np.mean(self.model.actuator_ctrlrange, axis=1)
+        self.act_rng = 0.5 * (self.model.actuator_ctrlrange[:,1] - self.model.actuator_ctrlrange[:,0])
+        print(self.act_mid, self.act_rng)
+        nu = len(self.act_rng)
+        self.action_space = Box(low=-1., high=1., shape=(nu,), dtype=np.float32)
+
+        ob = self.get_obs(self.data)
+        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(len(ob),), dtype=np.float32)
+
+    def reset(self):
+        self.sim.reset()
+        ob = self.get_obs(self.data)
+        return ob
+
+
+    def get_obs(self, data):
+        return np.concatenate([data.qpos.ravel(), data.qvel.ravel()])
+
+    def print_status(self):
+
+        # print('forward vel : {:.2f}'.format(self.data.qvel[0]))
+        t_rpy = euler2quat(np.array([0., 0., 2.*np.pi]))
+        rot = self.data.body_xquat[self.chassis_bid]
+
+        print('rot : ', t_rpy, rot)
+
+    def get_rew(self, data):
+        # rot = data.body_xmat[self.chassis_bid].reshape(3,3)
+        rot = data.body_xquat[self.chassis_bid]
+        # rpy = mat2quat(rot)
+        rpy = rot
+        # t_rpy = mat2quat(np.eye(3))
+        # t_rpy = np.array([1., 0., 0., 0.])
+        t_rpy = euler2quat(np.array([0., 0., 2.*np.pi]))
+        jnts = [data.qpos[jnt_addr] for jnt_addr in self.motor_bids]
+        ext  = [data.qpos[jnt_addr] for jnt_addr in self.extension_bids]
+        # return (data.qvel[0]-0.6)**2 + 10*np.sum((rpy-t_rpy)**2) #np.sum(data.qpos[:3]**2) #+ 10.*data.qpos[3]**2
+        # return -data.qvel[2]# + 100*np.sum((rpy-t_rpy)**2)
+        return 0*data.qvel[0] - np.sum((rpy-t_rpy)**2) #+ data.qvel[2]**2# + 1e-3*np.sum(np.square(jnts)) + 1e-3*np.sum(np.square(ext))
+
+    # def get_rew(self, data):
+    #     rot = data.body_xmat[self.chassis_bid].reshape(3,3)
+    #     rpy = mat2euler(rot)
+    #     t_rpy = mat2quat(np.eye(3))
+    #     jnts = [data.qpos[jnt_addr] for jnt_addr in self.motor_bids]
+    #     ext  = [data.qpos[jnt_addr] for jnt_addr in self.extension_bids]
+    #     # return (data.qvel[0]-0.6)**2 + 10*np.sum((rpy-t_rpy)**2) #np.sum(data.qpos[:3]**2) #+ 10.*data.qpos[3]**2
+    #     # return -data.qvel[2]# + 100*np.sum((rpy-t_rpy)**2)
+    #     # return (data.qvel[0]-2)**2 + np.sum((rpy-t_rpy)**2) #+ data.qvel[2]**2# + 1e-3*np.sum(np.square(jnts)) + 1e-3*np.sum(np.square(ext))
+    #     return (rpy[0] - np.pi * 2)**2
+
+
+    def step(self, a):
+
+        ctrl = np.clip(a, -1.0, 1.0)
+        ctrl = self.act_mid + ctrl * self.act_rng
+
+        self.data.ctrl[:] = ctrl
+
+        self.sim.step()
+
+        ob = self.get_obs(self.data)
+        rew = self.get_rew(self.data)
+        done = False
+        return ob, rew, done, {}
+
+
+    def render(self):
+        self.viewer.render()
+
+    def get_state(self):
+        return self.sim.get_state()
---
+++ test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+from minitaur_env import MinitaurEnv
+from minitaur_multmodel import MinitaurMultModel
+from scipy.signal import savgol_filter
+
+import matplotlib.pyplot as plt
+
+def mppi(state, model, u_seq, horizon, lam=0.2, sig=0.6):
+    assert len(u_seq) == horizon
+
+    model.set_state(state)
+
+    s   = []
+    eps = []
+    eta = 0.
+    alpha = 0.4
+    for t in range(horizon):
+        eta = alpha * eta + (1-alpha) * np.random.normal(0., sig, size=(model.n_sims, model.action_space.shape[0]))
+        eps.append(eta)
+        obs, rew, done, _ = model.step(u_seq[t] + eps[-1])
+        s.append(rew)
+
+    s = np.cumsum(s[::-1], 0)[::-1, :]
+
+    for t in range(horizon):
+        s[t] -= np.max(s[t])
+        w = np.exp(s[t]/lam) + 1e-8 # offset
+        w /= np.sum(w)
+        u_seq[t] = u_seq[t] + np.dot(w, eps[t])
+    # return savgol_filter(u_seq, horizon-1, 3, axis=0)
+    return u_seq
+
+env = MinitaurEnv()
+model = MinitaurMultModel(n_sims=50)
+
+
+env.reset()
+model.reset()
+
+horizon = 10
+
+num_actions = env.action_space.shape[0]
+
+u_seq = [np.zeros(model.action_space.shape[0]) for _ in range(horizon)]
+
+t = 0
+# plt.ion()
+
+while True:
+
+    state = env.get_state()
+    u_seq = mppi(state, model, u_seq, horizon)
+
+    # obs, rew, done, _ = env.step(u_seq[0]*0 + np.array([0.]*4 + [-0]*4) )
+    # obs, rew, done, _ = env.step(u_seq[0]*0 + np.sin(np.pi*t/30))
+    obs, rew, done, _ = env.step(u_seq[0])
+    env.render()
+    u_seq[:-1] = u_seq[1:]
+    u_seq[-1]  = np.zeros(env.action_space.shape[0])
+    # plt.clf()
+    # plt.plot(u_seq)
+    # plt.draw()
+    # plt.pause(0.001)
+    # print(env.sim.data.qpos[:3])
+
+    t += 1
+
+    env.print_status()
